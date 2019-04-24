5617d878ef4ad75f46b34727d5c5fa8092414e22
e102y <e102y@users.noreply.github.com>
2019-04-23 21:52:09 -0700

    Add files via upload

--- Source/PuzzleGame/Interactable.h
+++ Source/PuzzleGame/Interactable.h
@@ -22,9 +22,9 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	//UFunction didn't play well with the virtual keyword :(
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
-	//virtual void Interact();
-	void Interact();
+	//UFunction didn't play well with the virtual keyword :
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+    //virtual void Interact();
+    void Interact();
 
 };
